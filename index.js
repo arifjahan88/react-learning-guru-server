1b86339ca0ef6a39245ecb81f74eebbcb9f72b22
Arif Jahan <arifjahan88@gmail.com>
2022-10-25 16:55:11 +0600

    after data load

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+
+const courseCategories = require("./Data/courseCatagories.json");
+const courseDetails = require("./Data/coursesdetails.json");
 
 app.get("/", (req, res) => {
   res.send("Api is Running");
+});
+app.get("/coursecategories", (req, res) => {
+  res.send(courseCategories);
 });
 
 app.listen(port, () => {
